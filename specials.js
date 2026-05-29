fae8c3ce2e8731b1b9010cadd014016fde05af57
deonshaymerritt-debug <deonshaymerritt@gmail.com>
2026-05-28 19:26:12 -0500

    Add special abilities from specials.js

---
+++ specials.js
@@ -0,0 +1,88 @@
+window.specials = [
+
+{
+
+name: "Upgrade",
+
+category: "Buff_Static",
+
+value: 1,
+
+description: "Adds +1 to every stat",
+
+image: "https://drive.google.com/uc?id=1bhBcAyNuCMgANLsC2i4YeEnCq36mcQHA"
+
+},
+
+{
+
+name: "Injury",
+
+category: "Debuff_Static",
+
+value: -1,
+
+description: "Subtracts -1 from every stat",
+
+image: "https://drive.google.com/uc?id=1x-sMgAKXBb6tzYNJz_bQSwMRaqvYvz20"
+
+},
+
+{
+
+name: "Plus Ultra",
+
+category: "Buff_Multiplier",
+
+value: 2,
+
+description: "Doubles all stats",
+
+image: "https://drive.google.com/uc?id=13f_UYpdeJxyoKeQcLCus1Nljsr3d1DCW"
+
+},
+
+{
+
+name: "Error",
+
+category: "Debuff_Multiplier",
+
+value: 0.5,
+
+description: "Cuts all stats in half",
+
+image: "https://drive.google.com/uc?id=1sBnSpJtDctuf2WCcc3rLesiLHzgwrPVG"
+
+},
+
+{
+
+name: "Jump Force",
+
+category: "Utility_Expansion",
+
+value: 1,
+
+description: "Adds an extra character slot",
+
+image: "https://drive.google.com/uc?id=1sw6yQUUez76CI8eSHqWe1k4UEmVSIzKm"
+
+},
+
+{
+
+name: "Author Abilities",
+
+category: "Utility_Manipulation",
+
+value: null,
+
+description: "Swaps card placements",
+
+image: "https://drive.google.com/uc?id=1ocedi9F3YwUeflVXC7e08lLZ58kFtDp1"
+
+}
+
+]; 
+
